9e12f62351d40f9b5db9fb4f353e8bf0129a072e
sandesh300 <bhujbalsandesh52@gmail.com>
2024-08-10 23:52:47 +0530

    create movie model and MovieRepository

---
+++ src/main/java/com/movieflix/repositories/MovieRepository.java
@@ -0,0 +1,8 @@
+package com.movieflix.repositories;
+
+import com.movieflix.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+}
