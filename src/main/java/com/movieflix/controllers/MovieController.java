0a14e1e9bef2714889f0ee6276411f6f773c6afc
sandesh300 <bhujbalsandesh52@gmail.com>
2024-08-21 22:54:55 +0530

    create MovieController.java and add Post, Get methods

---
+++ src/main/java/com/movieflix/controllers/MovieController.java
@@ -0,0 +1,49 @@
+package com.movieflix.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.movieflix.dto.MovieDto;
+import com.movieflix.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/add-movie")
+    public ResponseEntity<MovieDto> addMovieHandler(@RequestPart MultipartFile file,
+                                                    @RequestPart String movieDto) throws IOException {
+
+        MovieDto dto = convertToMovieDto(movieDto);
+        return new ResponseEntity<>(movieService.addMovie(dto, file), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{movieId}")
+    public ResponseEntity<MovieDto> getMovieHandler(@PathVariable Integer movieId) {
+        return ResponseEntity.ok(movieService.getMovie(movieId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieDto>> getAllMovieHandler() {
+        return ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+
+
+    private MovieDto convertToMovieDto(String movieDtoObj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(movieDtoObj, MovieDto.class);
+    }
+
+}
