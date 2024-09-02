99e3ee5908eb3702dc6bd6425dbe3a5e4226b676
sandesh300 <bhujbalsandesh52@gmail.com>
2024-09-02 17:15:46 +0530

    add MoviePageResponse.java

---
+++ src/main/java/com/movieflix/dto/MoviePageResponse.java
@@ -0,0 +1,12 @@
+package com.movieflix.dto;
+
+import java.util.List;
+
+public record MoviePageResponse(List<MovieDto> movieDtos,
+                                Integer pageNumber,
+                                Integer pageSize,
+                                long totalElements,
+                                int totalPages,
+                                boolean isLast
+                                ) {
+}
