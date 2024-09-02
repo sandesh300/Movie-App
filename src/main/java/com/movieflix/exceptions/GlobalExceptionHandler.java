61493cb928845cb3315173a6e00c1deab81485c0
sandesh300 <bhujbalsandesh52@gmail.com>
2024-09-02 15:46:42 +0530

    add GlobalExceptionHandler.java to handle all the exception

---
+++ src/main/java/com/movieflix/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.movieflix.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    public ProblemDetail handleMovieNotFoundException(MovieNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(FileExistsException.class)
+    public ProblemDetail handlerFileExistsException(FileExistsException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(EmptyFileException.class)
+    public ProblemDetail handlerEmptyFileException(EmptyFileException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+}
