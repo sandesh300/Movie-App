a40c4e1b71b0ccbc0218bb3a0abacae00905e4e9
sandesh300 <bhujbalsandesh52@gmail.com>
2024-09-14 23:00:00 +0530

    update code in User.java

--- src/main/java/com/movieflix/auth/models/User.java
+++ src/main/java/com/movieflix/auth/models/User.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -26,42 +27,35 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
-    @NotBlank(message = "The name field can't be blank!")
+    @NotBlank(message = "The name field can't be blank")
     private String name;
 
-    @NotBlank(message = "The username field can't be blank!")
+    @NotBlank(message = "The username field can't be blank")
     @Column(unique = true)
     private String username;
 
-    @NotBlank(message = "The email field can't be blank!")
+    @NotBlank(message = "The email field can't be blank")
     @Column(unique = true)
     @Email(message = "Please enter email in proper format!")
     private String email;
 
-    @NotBlank(message = "The password field can't be blank!")
-    @Size(min = 5, message = "The password must have five characters!")
+    @NotBlank(message = "The password field can't be blank")
+    @Size(min = 5, message = "The password must have at least 5 characters")
     private String password;
 
     @OneToOne(mappedBy = "user")
     private RefreshToken refreshToken;
 
+//    @OneToOne(mappedBy = "user")
+//    private ForgotPassword forgotPassword;
+
     @Enumerated(EnumType.STRING)
     private UserRole role;
-
-    private boolean isEnabled = true;
-
-    private boolean isAccountNonExpired = true;
-
-    private boolean isAccountNonLocked = true;
-
-    private boolean isCredentialsNonExpired = true;
-
-
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
@@ -76,21 +70,21 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return isAccountNonExpired;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return isAccountNonLocked;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return isCredentialsNonExpired;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return isEnabled;
+        return true;
     }
 }
