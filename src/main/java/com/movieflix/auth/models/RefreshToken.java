364556b1a6c9115279c3a303d8b53e2793a92c87
sandesh300 <bhujbalsandesh52@gmail.com>
2024-09-15 14:09:25 +0530

    add RefreshToken.java for the JWT authentication

---
+++ src/main/java/com/movieflix/auth/models/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.movieflix.auth.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer tokenId;
+
+    @Column(nullable = false, length = 500)
+    @NotBlank(message = "Please enter refreshToken value!")
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Instant expirationTime;
+
+    @OneToOne
+    private User user;
+
+}
